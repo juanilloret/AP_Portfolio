a07dca4b2d35568ddd2798a003e735218f9d4838
juanilloret95@gmail.com <juanilloret95@gmail.com>
2022-06-20 03:46:50 -0300

    commit bootstrap grid

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent
+    SocialComponent,
+    BannerComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
