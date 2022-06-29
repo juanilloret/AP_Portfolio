d76145f305d7be7b4b906fd4533103c1fcec44e3
juanilloret95@gmail.com <juanilloret95@gmail.com>
2022-06-29 17:43:05 -0300

    commit

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+//BrowserAnimationsModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,6 +9,10 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,16 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgCircleProgressModule.forRoot({/*set defaults here*/})//modulo de circulos de progreso animados
   ],
   providers: [],
   bootstrap: [AppComponent]
