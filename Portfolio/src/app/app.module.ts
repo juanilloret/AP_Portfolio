1b912b112dc823f49e20ed9fbb7ee6c7f719cf7a
juanilloret95@gmail.com <juanilloret95@gmail.com>
2022-07-01 00:11:18 -0300

    commit

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HabilidadesComponent } from './components/habilidades/habilidades.compo
     AboutMeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HabilidadesComponent
+    HabilidadesComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
